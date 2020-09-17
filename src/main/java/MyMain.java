42037b22adb499c5e15e4bd06dc20f4bd96a0851
21cadams <21cadams@athenian.org>
2020-09-17 03:24:26 +0000

    Decimal to Binary

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,27 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int binary2=Integer.parseInt(binary);
+        double counter = 0;
+        int holder = 0;
+        int total = 0;
+        while(binary2 > 0){
+            holder = binary2 % 2;
+            total += holder * Math.pow(2.0, counter);
+            binary2 = binary2 / 10;
+            counter++;
+        }
+        System.out.println("This is equal to the decimal value " + total);
+        return total;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write your number"); 
+        String binary = scan.nextLine(); 
+        binaryToDecimal(binary);
+        scan.close();
     }
 }
